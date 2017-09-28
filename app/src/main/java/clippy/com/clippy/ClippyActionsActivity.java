760e2ea4d87bb63e90bcc70f5c504f7995062cc7
David Herman <davidherman@google.com>
2017-09-28 13:50:35 -0700

    Clean up file names and labels

--- app/src/main/java/clippy/com/clippy/ClippyActionsActivity.java
+++ app/src/main/java/clippy/com/clippy/ClippyActionsActivity.java
@@ -3,11 +3,11 @@ package clippy.com.clippy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ChatActivity extends AppCompatActivity {
+public class ClippyActionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_clippy_actions);
     }
 }
